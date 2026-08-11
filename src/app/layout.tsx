876d1f527c23efcf7513ec741a984bf51af25f66
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-11 14:46:16 -0500

    feat: load Greenatics company story styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./brand.css";
 import "./solutions.css";
 import "./tech-impact.css";
 import "./diagnostic.css";
+import "./about.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { site } from "@/data/site";
