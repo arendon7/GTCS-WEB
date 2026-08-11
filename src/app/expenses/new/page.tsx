0d6b8309ef583f41eb2ee2249f18cda783d0577e
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-11 17:26:21 -0500

    feat(expenses): add new expense route

---
+++ src/app/expenses/new/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { ExpenseForm } from "@/components/expense-form";
+
+export default function NewExpensePage(){
+  return <AppShell><ExpenseForm/></AppShell>;
+}
