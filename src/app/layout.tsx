793a4643d34f4b2c0c5264a09aec484addb8157d
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-11 14:27:46 -0500

    feat: load technology and impact visual layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./commerce.css";
 import "./brand.css";
 import "./solutions.css";
+import "./tech-impact.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { site } from "@/data/site";
